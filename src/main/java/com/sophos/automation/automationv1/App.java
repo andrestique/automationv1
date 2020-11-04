2ea80a5ad85e0ceb6ea022803f8c7cfadb377ab7
Andres Humberto  Tique Cifuentes <andres.tique@sophossolutions.com>
2020-11-04 17:14:22 -0500

    Segundo commit

--- src/main/java/com/sophos/automation/automationv1/App.java
+++ src/main/java/com/sophos/automation/automationv1/App.java
@@ -1,8 +1,6 @@
 package com.sophos.automation.automationv1;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 /**
  * Hello world!
@@ -12,9 +10,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        String variable = "12345";
-        
-        WebDriver driver = new ChromeDriver();
+    	int edad = 12;
+    	
+    	if(edad>=18) {
+    		System.out.println("La persona es mayor de edad");
+    	}else {
+    		System.out.println("La persona es menor de edad");
+    	}
     }
 }

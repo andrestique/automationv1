2e875c6add57ebc755d45966af80640aec9c5147
Andres Humberto  Tique Cifuentes <andres.tique@sophossolutions.com>
2020-11-04 12:40:23 -0500

    primer commit

---
+++ src/main/java/com/sophos/automation/automationv1/App.java
@@ -0,0 +1,20 @@
+package com.sophos.automation.automationv1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String variable = "12345";
+        
+        WebDriver driver = new ChromeDriver();
+    }
+}
